6ddba0990d52355adbdccc5fae1c360d366ff480
f76goat <indigolucas69@gmail.com>
2025-06-20 11:36:50 -0400

    I_think_something_is_broken

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,15 +1,6 @@
 #include <test_header_include_all.h>
 #include "x86_64/kernel_include_test_header.h"
 
-const char *msg_test = " Test!";
-
-void test_divide_by_zero(void)
-{
-    volatile int a = 1;
-    volatile int b = 0;
-    volatile int c = a / b;
-}
-
 void _kmain(void)
 {
     vga_clear_screen();
@@ -20,13 +11,6 @@ void _kmain(void)
     }
 
     idt_init();
-
-    test_divide_by_zero();
-
-    const char *msg_2 = " Works!";
-    for (int i = 0; msg_2[i] != '\0'; ++i) {
-        vga_putc(msg_2[i]);
-    }
 
     for (;;)
         __asm__ volatile ("hlt");
